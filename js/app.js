c68956f69dc8ea5e2c56bb59e230d87821eeda9d
razawi <raz.kronenberg@gmail.com>
2016-02-08 02:55:57 +0200

    routing skelaton

--- js/app.js
+++ js/app.js
@@ -2,8 +2,8 @@ angular.module('flashCardsApp', ['ngResource', 'ngRoute'])
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider
             .when('/', {
-                controller:  'cards',
-                templateUrl: 'http://127.0.0.1:8888/public/apps/flashCards-client/Partials/CardList.html'
+                controller:  'cardlist',
+                templateUrl: 'http://127.0.0.1:8888/public/apps/flashCards-client/Partials/CardsList.html'
             })
             .otherwise('/')
     }]);
--- js/controllers/cards.js
+++ js/controllers/cards.js
@@ -1,10 +1,10 @@
 angular.module('flashCardsApp')
     .controller('cardlist', ['$scope', '$http', function($scope, $http) {
 
-        //$http.get('http://127.0.0.1:8888/dbapi/languages').success(function(response){
-        //    console.log('$scope.list at getMovies ');
-        //    $scope.langlist = response;
-        //})
+        $http.get('http://127.0.0.1:8888/api/listallcards').success(function(response){
+            $scope.cardslist = response;
+            console.log("$scope.cardslist : " + $scope.cardslist.toString());
+        })
 
         console.log("Hello World from tableManage");
     }]);
