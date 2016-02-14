44088a96e675cb5ba557fa1bbdd32445bc8d8d8d
razawi <raz.kronenberg@gmail.com>
2016-02-15 01:43:56 +0200

    testing deployment TBD: change urls from hardcodded to config files

--- js/controllers/card.js
+++ js/controllers/card.js
@@ -1,7 +1,7 @@
 angular.module('flashCardsApp')
     .controller('cardlist', ['$scope', '$http', function($scope, $http) {
         // param
-        $http.get('http://127.0.0.1:8888//api/cards/card:cardid').success(function(response){
+        $http.get('https://fierce-taiga-41516.herokuapp.com/api/cards/card:cardid').success(function(response){
             $scope.cardslist = response;
             console.log("$scope.cardslist : " + $scope.cardslist.toString());
         })
--- js/controllers/cards.js
+++ js/controllers/cards.js
@@ -1,7 +1,7 @@
 angular.module('flashCardsApp')
     .controller('cardlist', ['$scope', '$http', function($scope, $http) {
 
-        $http.get('http://127.0.0.1:8888/api/listallcards').success(function(response){
+        $http.get('https://fierce-taiga-41516.herokuapp.com/api/listallcards').success(function(response){
             $scope.cardslist = response;
             console.log("$scope.cardslist : " + $scope.cardslist.toString());
         })
