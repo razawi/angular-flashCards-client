41edc433a4dcf5dc38d7fb360d5620a8ad9a7c1e
razawi <raz.kronenberg@gmail.com>
2016-02-15 01:35:39 +0200

    for heroku deployment

--- js/app.js
+++ js/app.js
@@ -3,11 +3,11 @@ angular.module('flashCardsApp', ['ngResource', 'ngRoute'])
         $routeProvider
             .when('/', {
                 controller:  'cardlist',
-                templateUrl: 'http://127.0.0.1:8888/public/apps/flashCards-client/Partials/CardsList.html'
+                templateUrl: 'https://fierce-taiga-41516.herokuapp.com/public/apps/flashCards-client/Partials/CardsList.html'
             })
             .when('/cardview:id', {
                 controller:  'cardview',
-                templateUrl: 'http://127.0.0.1:8888/public/apps/flashCards-client/Partials/CardView.html'
+                templateUrl: 'https://fierce-taiga-41516.herokuapp.com/public/apps/flashCards-client/Partials/CardView.html'
             })
             .otherwise('/')
     }]);
