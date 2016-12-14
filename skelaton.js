4d6084cd99998bd35a2530d6bc4ec541e0cfa30b
razawi <raz.kronenberg@gmail.com>
2016-12-14 23:33:24 +0200

    basic API ajax call and display. needs more styling and <integration

--- skelaton.js
+++ skelaton.js
@@ -1,3 +1,18 @@
 $( document ).ready(function() {
     console.log( "ready!" );
+
+    $.getJSON( 'https://fierce-taiga-41516.herokuapp.com/api/listallcategorys', function( data ) {
+    var items = [];
+
+
+    $.each( data, function( key, val ) {
+        debugger;
+        items.push( "<li id='" + JSON.stringify(key) + "'>" + JSON.stringify(val) + "</li>" );
+    });
+    
+    $( "<ul/>", {
+        "class": "my-new-list",
+        html: items.join( "" )
+        }).appendTo( "body" );
+      });
 });
