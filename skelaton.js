869c62017ded66571e3c14e0d2d82e01278bfead
razawi <raz.kronenberg@gmail.com>
2016-12-15 00:30:54 +0200

    styling with lodash

--- skelaton.js
+++ skelaton.js
@@ -1,18 +1,36 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
 
-    $.getJSON( 'https://fierce-taiga-41516.herokuapp.com/api/listallcategorys', function( data ) {
-    var items = [];
+    drawCategories();
 
-
-    $.each( data, function( key, val ) {
-        debugger;
-        items.push( "<li id='" + JSON.stringify(key) + "'>" + JSON.stringify(val) + "</li>" );
-    });
-    
-    $( "<ul/>", {
-        "class": "my-new-list",
-        html: items.join( "" )
-        }).appendTo( "body" );
-      });
 });
+
+
+function drawCategories(){
+    $.getJSON( 'https://fierce-taiga-41516.herokuapp.com/api/listallcategorys', function( data ) {
+        var items = [];
+        debugger;
+
+        // var array = [1];
+        // var other = _.concat(array, 2, [3], [[4]]); 
+        // console.log(other);
+        // var odata =_.omit(data[0], ["_id", "__v"])
+
+        // deep parse
+        $.each( data, function( key, val ) {
+            items.push( "<li id='" + JSON.stringify(key) + "'>" + JSON.stringify(_.omit(val, ["_id", "__v"] )) + "</li>" );
+        });
+        
+        $( "<ul/>", {
+            "class": "my-new-list",
+            html: items.join( "" )
+        }).appendTo( "body" );
+    });
+}
+
+function drawCurricula(){
+    
+}
+
+function drawCards(){
+
+}
