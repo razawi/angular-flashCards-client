45441685bbef98e996f8a45eab37bd8f214fe0e8
razawi <raz.kronenberg@gmail.com>
2016-12-16 04:07:17 +0200

    curriculas data parsed, next step make dynamic HTML tabes

--- skelaton.js
+++ skelaton.js
@@ -1,14 +1,64 @@
 $( document ).ready(function() {
 
-    drawCategories();
+    drawCurricula();
 
 });
 
 
-function drawCategories(){
-    $.getJSON( 'https://fierce-taiga-41516.herokuapp.com/api/listallcategorys', function( data ) {
-        var items = [];
+function drawCurricula(){
+        $.getJSON( 'http://127.0.0.1:8888/api/curriculaList', function( data ) {
+
+        function faceMapper(face){
+            return _.pick(face, 'symbol', 'text');
+        }
+
+        function mapper(card){
+            dispData={};
+            dispData.name = _.pick(card, 'name');
+            dispData.facess = _.map(card.facess, faceMapper);
+
+            return dispData;
+        }
+
+        var displayData = _.map(data, mapper)
+
+
+        var headers = [];
+        var content = [];
+
         debugger;
+
+
+        // Dynamic table display 
+
+        $.each( dispData, function( key, val ) {
+            if (key === "facess"){
+                $.each( val, function( num, face ) { 
+                    debugger;
+                });
+            }
+            else
+                items.push( "<li id='" + key + "'>" + val + "</li>" );
+        });
+        
+        $( "<ul/>", {
+            "class": "my-new-list",
+            html: items.join( "" )
+        }).appendTo( "body" );
+    });
+
+}
+
+function drawCards(){
+    // http://127.0.0.1:8888/api/cardsList
+}
+
+function drawCategories(){ // server side bug in categories
+    $.getJSON( 'http://127.0.0.1:8888/api/categoriesList', function( data ) {
+        var items = [];
+
+        var headers = [];
+        var content = [];
 
         // var array = [1];
         // var other = _.concat(array, 2, [3], [[4]]); 
@@ -25,12 +75,4 @@ function drawCategories(){
             html: items.join( "" )
         }).appendTo( "body" );
     });
-}
-
-function drawCurricula(){
-    
-}
-
-function drawCards(){
-
 }
