825c508b158ef4afbba34841f064e09e7af3d0fb
razawi <raz.kronenberg@gmail.com>
2016-12-16 05:38:09 +0200

    cards dynamic table displayed

--- skelaton.js
+++ skelaton.js
@@ -1,22 +1,21 @@
 $( document ).ready(function() {
 
-    drawCurricula();
+    drawCards();
 
 });
 
+function mapper(card){
+    dispData={};
+    dispData = _.pick(card, 'name');
+    dispData.facess = _.map(card.facess, function (face){
+        return _.pick(face, 'symbol', 'text');
+    });
 
-function drawCurricula(){
-    $.getJSON( 'http://127.0.0.1:8888/api/curriculaList', function( data ) {
+    return dispData;
+}
 
-        function mapper(card){
-            dispData={};
-            dispData = _.pick(card, 'name');
-            dispData.facess = _.map(card.facess, function (face){
-                return _.pick(face, 'symbol', 'text');
-            });
-
-            return dispData;
-        }
+function drawCards(){
+     $.getJSON('http://127.0.0.1:8888/api/cardsList', function( data ) {
 
         var displayData = _.map(data, mapper)
 
@@ -31,7 +30,6 @@ function drawCurricula(){
        });
 
        $.each( displayData, function( key, card ) { 
-           debugger;
            lines += '<tr> <td> ' + card.name + '</td>';
            $.each( card.facess, function( num, face ) { 
                 lines += '<td> ' + face.text + '</td>'; 
@@ -40,7 +38,41 @@ function drawCurricula(){
        });
 
        var table = headTempl + titles + seperator + lines + footer;
+       $( "<table/>", {
+           id : "curiculaTable",
+           class: "table table-striped",
+           html: table
+       }).appendTo( "body" );
+     });
 
+    
+}
+
+
+function drawCurricula(){
+    $.getJSON( 'http://127.0.0.1:8888/api/curriculaList', function( data ) {
+
+        var displayData = _.map(data, mapper)
+
+        var headTempl =  '<table id="curiculaTable" class="table table-striped"> <thead> <tr>';
+        var seperator =  '</thead>  <tbody> '; 
+        var footer = '</tbody>  </table>';
+        var titles = '<th>Name</th> ';
+        var lines =  ''; 
+
+       $.each( displayData[0].facess, function( key, val ) {
+            titles += ' <th> ' + val.symbol + ' </th> '
+       });
+
+       $.each( displayData, function( key, card ) { 
+           lines += '<tr> <td> ' + card.name + '</td>';
+           $.each( card.facess, function( num, face ) { 
+                lines += '<td> ' + face.text + '</td>'; 
+           });
+           lines+="</tr>";
+       });
+
+       var table = headTempl + titles + seperator + lines + footer;
        $( "<table/>", {
            id : "curiculaTable",
            class: "table table-striped",
@@ -50,9 +82,7 @@ function drawCurricula(){
     });
 }
 
-function drawCards(){
-    // http://127.0.0.1:8888/api/cardsList
-}
+
 
 function drawCategories(){ // server side bug in categories
     $.getJSON( 'http://127.0.0.1:8888/api/categoriesList', function( data ) {
