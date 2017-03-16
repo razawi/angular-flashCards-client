06160a4af0fd44a3744f513670d608db6fe6b940
Raz Kronenberg <raz.kronenberg@gmail.com>
2017-03-16 13:47:06 +0200

    adding wix tree

--- curriculum/CurriculumCtrl.js
+++ curriculum/CurriculumCtrl.js
@@ -1,4 +1,4 @@
-app.controller('CurriculumCtrl', ['$scope', '$http', function($scope, $http) {
+app.controller('CurriculumCtrl', ['$scope', '$http',  function($scope, $http) {
    function mapper(card){
         dispData={};
         dispData = _.pick(card, 'name', '', '');
@@ -9,10 +9,42 @@ app.controller('CurriculumCtrl', ['$scope', '$http', function($scope, $http) {
         return dispData;
     }
 
-    $http.get('http://127.0.0.1:8888/api/curriculaList').success(function(response){
+    var apiUrl = 'https://better-flash-cards-api.herokuapp.com/api/'; //todo main configuration file
+
+    $http.get(apiUrl + 'curriculaList').success(function(response){
          $scope.curics = _.map(response, mapper)
           debugger;
     });
+
+// $scope.treeOptions = {
+//     nodeChildren: "children",
+//     dirSelectable: true,
+//     injectClasses: {
+//         ul: "a1",
+//         li: "a2",
+//         liSelected: "a7",
+//         iExpanded: "a3",
+//         iCollapsed: "a4",
+//         iLeaf: "a5",
+//         label: "a6",
+//         labelSelected: "a8"
+//     }
+// }
+// $scope.dataForTheTree =
+// [
+//     { "name" : "Joe", "age" : "21", "children" : [
+//         { "name" : "Smith", "age" : "42", "children" : [] },
+//         { "name" : "Gary", "age" : "21", "children" : [
+//             { "name" : "Jenifer", "age" : "23", "children" : [
+//                 { "name" : "Dani", "age" : "32", "children" : [] },
+//                 { "name" : "Max", "age" : "34", "children" : [] }
+//             ]}
+//         ]}
+//     ]},
+//     { "name" : "Albert", "age" : "33", "children" : [] },
+//     { "name" : "Ron", "age" : "29", "children" : [] }
+// ];
+
 
 //    $.each( displayData[0].facess, function( key, val ) {
 //         titles += ' <th> ' + val.symbol + ' </th> '
