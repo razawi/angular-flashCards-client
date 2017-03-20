45692565dcd86c72a4c2f807f6b07ae3848eb03a
Raz Kronenberg <raz.kronenberg@gmail.com>
2017-03-20 11:37:11 +0200

    basic Curriculum template

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-var app = angular.module('app', ['ui.router', 'treeControl']);
+var app = angular.module('app', ['ui.router']);
--- curriculum/CurriculumCtrl.js
+++ curriculum/CurriculumCtrl.js
@@ -1,6 +1,8 @@
 app.controller('CurriculumCtrl', ['$scope', '$http',  function($scope, $http) {
    function mapper(card){
         dispData={};
+
+        // todo - add description, categories
         dispData = _.pick(card, 'name', '_id', '');
         dispData.facess = _.map(card.facess, function (face){
             return _.pick(face, 'symbol', 'text');
@@ -12,46 +14,7 @@ app.controller('CurriculumCtrl', ['$scope', '$http',  function($scope, $http) {
     var apiUrl = 'https://better-flash-cards-api.herokuapp.com/api/'; //todo main configuration file
 
     $http.get(apiUrl + 'curriculaList').success(function(response){
-         $scope.curics = _.map(response, mapper)
-         
-
-
-
-
-          debugger;
+         $scope.curics = _.map(response, mapper);
     });
-
-// $scope.treeOptions = {
-//     nodeChildren: "children",
-//     dirSelectable: true,
-//     injectClasses: {
-//         ul: "a1",
-//         li: "a2",
-//         liSelected: "a7",
-//         iExpanded: "a3",
-//         iCollapsed: "a4",
-//         iLeaf: "a5",
-//         label: "a6",
-//         labelSelected: "a8"
-//     }
-// }
-
-// $scope.dataForTheTree =
-// [
-//     { "name" : "Joe", "age" : "21", "children" : [
-//         { "name" : "Smith", "age" : "42", "children" : [] },
-//         { "name" : "Gary", "age" : "21", "children" : [
-//             { "name" : "Jenifer", "age" : "23", "children" : [
-//                 { "name" : "Dani", "age" : "32", "children" : [] },
-//                 { "name" : "Max", "age" : "34", "children" : [] }
-//             ]}
-//         ]}
-//     ]},
-//     { "name" : "Albert", "age" : "33", "children" : [] },
-//     { "name" : "Ron", "age" : "29", "children" : [] }
-// ];
-
-
-
 
 }]);
