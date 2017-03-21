670e308edc410e77270efa64a2674f8796a2c9e0
razawi <raz.kronenberg@gmail.com>
2017-03-21 19:47:11 +0200

    skelaton extended to curics and categories, pre server changes

--- curricula/CurriculaCtrl.js
+++ curricula/CurriculaCtrl.js
@@ -7,7 +7,7 @@ app.controller('CurriculaCtrl', ['$scope', '$http', function($scope, $http) {
 
    function catMapper(card){
         dispData={};
-        dispData = _.pick(card, 'symbol', '', '');
+        dispData = _.pick(card, 'symbol', '_id', '');
         dispData.facess = _.map(card.facess, function (face){
             return _.pick(face, 'symbol', 'text');
         });
@@ -15,31 +15,11 @@ app.controller('CurriculaCtrl', ['$scope', '$http', function($scope, $http) {
         return dispData;
     }
 
-    $http.get('http://127.0.0.1:8888/api/categoriesList').success(function(response){
+
+    $http.get('https://better-flash-cards-api.herokuapp.com/api/categoriesList').success(function(response){
         $scope.categories = _.map(response, catMapper)
         debugger;
 
     });
-
-    //    $.each( displayData[0].facess, function( key, val ) {
-    //         titles += ' <th> ' + val.symbol + ' </th> '
-    //    });
-
-    //    $.each( displayData, function( key, card ) { 
-    //        lines += '<tr> <td> ' + card.symbol + '</td>';
-    //        $.each( card.facess, function( num, face ) { 
-    //             lines += '<td> ' + face.text + '</td>'; 
-    //        });
-    //        lines+="</tr>";
-    //    });
-
-    //    var table = headTempl + titles + seperator + lines + footer;
-    //    $( "<table/>", {
-    //        id : "curiculaTable",
-    //        class: "table table-striped",
-    //        html: table
-    //    }).appendTo( "body" );
-    // });
-
 
 }]);
