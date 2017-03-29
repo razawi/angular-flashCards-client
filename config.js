20968fc89befda325d30ee05e2e83be9e9ca7bb5
Raz Kronenberg <raz.kronenberg@gmail.com>
2017-03-29 20:30:42 +0300

    step 4 on authantication client tutorial

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ var configData = {
     url:"https://better-flash-cards-api.herokuapp.com/api/",
     webUrl: "https://better-flash-cards-api.herokuapp.com/api/",
     localURL: "http://127.0.0.1/api",
-    localUIMock: ""
+    jwt: "http://127.0.0.1:3000/login"
 }
--- login/ctrl.js
+++ login/ctrl.js
@@ -1,6 +1,53 @@
-app.controller('LogInCtrl', ['$scope', '$http', '$loading',  function($scope, $http, $loading) {
+app.controller('LogInCtrl', ['$scope', '$http', '$loading', 'RandomUserFactory', 'UserFactory',  
+    function($scope, $http, $loading, RandomUserFactory, UserFactory) {
+
+    var vm = this;
+    vm.getRandomUser = getRandomUser;
+    vm.login = login;
+
+    function getRandomUser() {
+      RandomUserFactory.getUser().then(function success(response) {
+        vm.randomUser = response.data;
+      }, handleError);
+    }
+
+    function login(username, password) {
+      UserFactory.login(username, password).then(function success(response) {
+        vm.user = response.data;
+      }, handleError);
+    }
+
+    function handleError(response) {
+      alert('Error: ' + response.data);
+    }
+
+}]);
 
 
     
+  app.factory('RandomUserFactory', function RandomUserFactory($http) {
+    'use strict';
+    return {
+      getUser: getUser
+    };
 
-}]);
+    function getUser() {
+      return $http.get(configData.jwt + '/login');
+    }
+  });
+
+  app.factory('UserFactory', function UserFactory($http) {
+    'use strict';
+    return {
+      login: login
+    };
+
+    function login(username, password) {
+      return $http.post(configData.jwt + '/login', {
+        username: username,
+        password: password
+      });
+    }
+    
+ });
+
