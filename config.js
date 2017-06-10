fb1682f4ba8c283d569a7713f95f93fd8082302c
razawi <raz.kronenberg@gmail.com>
2017-06-10 22:26:06 +0300

    basic login

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ var configData = {
     pppurl:"http://127.0.0.1:8888/api",
     url: "http://127.0.0.1:8888/api",
     localURL: "http://127.0.0.1/api",
-    jwt: "http://127.0.0.1:3000"
+    jwt: "http://127.0.0.1:8888"
 }
